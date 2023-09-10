c2c5373c7aa63e83f4c49d65f251b819504de70d
Kally95 <kally-95@hotmail.com>
2023-09-10 12:49:24 +0100

    removed redundant return statement

--- data_structures/binary_tree.go
+++ data_structures/binary_tree.go
@@ -5,16 +5,16 @@ import (
 )
 
 type BtNode struct {
-	Data int
-	Left *BtNode
+	Data  int
+	Left  *BtNode
 	Right *BtNode
- }
- 
- func (root *BtNode)PreOrderTraversal(){
-	if root !=nil{
-	   fmt.Printf("%d ", root.Data)
-	   root.Left.PreOrderTraversal()
-	   root.Right.PreOrderTraversal()
+}
+
+func (root *BtNode) PreOrderTraversal() {
+	if root != nil {
+		fmt.Printf("%d ", root.Data)
+		root.Left.PreOrderTraversal()
+		root.Right.PreOrderTraversal()
 	}
-	return
- }
+
+}
